b5fd209800a2f853388baf528191483efd40d394
zironpro <dev@zironpro.ae>
2026-06-02 12:51:45 +0400

    feat: add Apple mobile web app title to metadata and update favicon for enhanced branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,13 @@ import { cn } from "@/lib/utils";
 import { WixProvider } from "@/lib/wix/provider";
 
 export const metadata: Metadata = {
-	title: "Al Rewaya Book World | Your Premier Islamic Bookstore",
+	title: "Al Rewaya Book World | Your Premier Bookstore in UAE",
 	description:
 		"Discover a curated collection of Islamic literature, academic texts, and classic books at Rewaya Book World.",
 
 	other: {
 		"facebook-domain-verification": "40qw6zil0ubg6k26nl0ku20oy70djy",
+		"apple-mobile-web-app-title": "Rewaya",
 	},
 };
 
